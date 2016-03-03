9070ce5e3ea3a4346a73e32cec8604f8baa5354f
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-03-03 14:55:34 +0545

    customer entry task completed

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -10,7 +10,8 @@ class Customer extends CI_Controller
 {
     function newCustomer()
     {
-        $d['d']=array();
+        echo "hello";
+        $d['d'] = array();
         $data['title'] = "Create Customer";
         $data['content'] = $this->load->view('pages/customers/newcustomer', $d, true);
         $this->parser->parse('template/page_template', $data);
