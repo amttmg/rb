34056117cfc0ea3ad9f7ddcc374cbc8439f4e66a
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-03-08 17:00:08 +0545

    family added successfully

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -10,7 +10,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Customer extends CI_Controller {
 
-    private $image_name="";
+    private $image_name="default.jpg";
+    private $images=array();
 
     public function __construct()
     {
@@ -44,13 +45,32 @@ class Customer extends CI_Controller {
         $this->form_validation->set_rules('aniversary_date', 'Aniversary date', 'trim|min_length[10]|max_length[10]');
         $this->form_validation->set_rules('dob', 'DOB', 'trim|required');
         $this->form_validation->set_rules('photo', 'Photo', 'callback_validate_image');
+        for ($i=1; $i <=5 ; $i++) 
+        { 
+            if ($this->input->post('faname'.$i)) 
+            {
+                $this->form_validation->set_rules('faphoto'.$i, 'Photo', 'callback_validate_image[faphoto'.$i.']');
+            }  
+        }
         $this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
         if ($this->form_validation->run() ==TRUE) 
         {
-            $this->customer->insert($this->image_name);
-            $this->session->set_flashdata('message', 'Customer added successfully !');
-            //echo("success fully inserted data");
-            redirect('customer/index','refresh');
+                    $this->db->trans_begin();
+                    $this->customer->insert($this->image_name);
+                    $this->customer->insert_family($this->images);
+                    if ($this->db->trans_status() === FALSE)
+                    {
+                        $this->db->trans_rollback();
+                    }
+                    else
+                    {
+                        $this->db->trans_commit();
+                        $this->session->unset_userdata('customer_id');
+                        $this->session->set_flashdata('message', 'Customer added successfully !');
+                        echo("success fully inserted data");
+                        //redirect('customer/index','refresh');
+                    }
+        
         } 
         else 
         {
@@ -101,56 +121,64 @@ class Customer extends CI_Controller {
     {
 
     }
-    public function validate_image()
+    public function validate_image($val,$image_name=null)
     {
-        $config['upload_path'] = './uploads/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size'] = '1000';
-
-        $this->load->library('upload', $config);
-
-        if ( ! $this->upload->do_upload('photo'))
+        if($image_name)
         {
+            $config['upload_path'] = './uploads/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['max_size'] = '1000';
 
-            $this->form_validation->set_message('validate_image',$this->upload->display_errors());
-            return false;
+            $this->load->library('upload', $config);
+
+            if ( ! $this->upload->do_upload($image_name))
+            {
+                if(stristr($this->upload->display_errors(),'You did not select a file to upload'))
+                {
+                    return true;
+                }
+                else
+                {
+                    $this->form_validation->set_message('validate_image',$this->upload->display_errors());
+                    return false;
+                }
+
+                
+            }
+            else
+            {
+                $this->images[]=$this->upload->data('file_name');
+               return true;
+            }
         }
         else
         {
-            $this->image_name=$this->upload->data('file_name');
-           return true;
-        }
-    }
+            $config['upload_path'] = './uploads/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['max_size'] = '1000';
 
-    public function insert_family()
-    {
-        $count=0;
-        $data=array();
-        for ($i=1; $i <=5 ; $i++) 
-        { 
-            if ($this->input->post('faname'.$i)) 
+            $this->load->library('upload', $config);
+
+            if ( ! $this->upload->do_upload('photo'))
             {
-                $count++;
-                $this->form_validation->set_rules('faname'.$i, 'Name', 'trim|required|min_length[2]|max_length[100]');
-                $this->form_validation->set_rules('faaddress'.$i, 'Address', 'trim|required|min_length[2]|max_length[100]');
-                $this->form_validation->set_rules('faphone'.$i, 'Name', 'trim|required|min_length[8]|max_length[15]');
-                $this->form_validation->set_rules('faphone2'.$i, 'Name', 'trim|min_length[8]|max_length[15]');
-                $this->form_validation->set_rules('farelation'.$i, 'Relation', 'trim|required|min_length[5]|max_length[15]');
-            }  
+                if(stristr($this->upload->display_errors(),'You did not select a file to upload'))
+                {
+                    return true;
+                }
+                else
+                {
+                    $this->form_validation->set_message('validate_image',$this->upload->display_errors());
+                    return false;
+                }
+                
+            }
+            else
+            {
+                $this->image_name=$this->upload->data('file_name');
+               return true;
+            }
         }
-        $this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
-       if ($this->form_validation->run() == FALSE) 
-       {
-            $data['priorities']=$this->mpriority->getPriority();
-            $data['title'] = "Create Customer";
-            $data['content'] = $this->load->view('pages/customers/newcustomer',$data, true);
-            $this->parser->parse('template/page_template', $data); 
-       } 
-       else 
-       {
-          echo("successfully validate");
-       }
-
+        
     }
 
     public function test()
