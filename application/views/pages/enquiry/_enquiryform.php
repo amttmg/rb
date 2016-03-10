560c1707944f134c9800d1e239ed8e59a90004c3
amttm <amt.tmg@gmail.com>
2016-03-10 14:55:06 +0545

    new enquiry

--- application/views/pages/enquiry/_enquiryform.php
+++ application/views/pages/enquiry/_enquiryform.php
@@ -1,72 +1,56 @@
 <div class="col-md-12">
-    <div class="col-md-8">
-        <table class="table table-bordered">
-            <tr>
-                <td>
-                    Full Name
-                </td>
-                <td>
-                    <?php echo $customer->fname . ' ' . $customer->mname . ' ' . $customer->lname ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Address
-                </td>
-                <td>
-                    <?php echo $customer->address ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Email
-                </td>
-                <td>
-                    <?php echo $customer->email ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Phone
-                </td>
-                <td>
-                    <?php echo $customer->phone1 ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Gender
-                </td>
-                <td>
-                    <?php echo $customer->gender ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Date of birth
-                </td>
-                <td>
-                    <?php echo $customer->dob ?>
-                </td>
-            </tr>
-            <?php if ($customer->marital_status) { ?>
-                <tr>
-                    <td>
-                        Anniversary Date
-                    </td>
-                    <td>
-                        <?php echo $customer->anniversary_date ?>
-                    </td>
-                </tr>
-            <?php } ?>
-        </table>
-    </div>
-    <div class="col-md-4">
-        <img class="image thumbnail"
-             src="<?php echo base_url('uploads/' . $customer->customer_image) ?>" height="200px">
+    <div class="box box-info">
+        <div class="box-header with-border">Customer Info</div>
+        <div class="box-body">
+            <div class="col-md-8">
+
+                <table class="table table-bordered">
+                    <tr>
+                        <td>
+                            Full Name
+                        </td>
+                        <td>
+                            <?php echo $customer->fname . ' ' . $customer->mname . ' ' . $customer->lname ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Address
+                        </td>
+                        <td>
+                            <?php echo $customer->address ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Email
+                        </td>
+                        <td>
+                            <?php echo $customer->email ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Phone
+                        </td>
+                        <td>
+                            <?php echo $customer->phone1 ?>
+                        </td>
+                    </tr>
+
+                </table>
+            </div>
+            <div class="col-md-4">
+                <img class="image thumbnail"
+                     src="<?php echo base_url('uploads/' . $customer->customer_image) ?>" height="150px">
+            </div>
+        </div>
     </div>
 </div>
-<hr>
+
 <div class="col-md-12">
-Hello Hello
+    <div class="box box-info">
+        <div class="box-header with-border">Enquiry Form</div>
+        <div class="box-body">Hello</div>
+    </div>
 </div>
