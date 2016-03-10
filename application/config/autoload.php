ef2add7b3f1a513d23a1f09129f9f8f2591b852f
Amrit <amt.tmg@gmail.com>
2016-03-11 02:25:33 +0545

    auto load notification helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','login');
+$autoload['helper'] = array('url','file','login', 'notification');
 
 /*
 | -------------------------------------------------------------------
