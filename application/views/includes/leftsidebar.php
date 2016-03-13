0abf22ff5c06965426ca735151165921f63456f3
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-03-13 15:55:41 +0545

    customer enquiry display successfully

--- application/views/includes/leftsidebar.php
+++ application/views/includes/leftsidebar.php
@@ -35,7 +35,7 @@
                 </a>
                 <ul class="treeview-menu">
                     <li class="list"><a href="<?php echo base_url('enquiry/newEnquiry') ?>"><i class="fa fa-plus"></i>New Enquery</a></li>
-                    <li class="list"><a href="<?php echo base_url('enquiry') ?>"><i class="fa fa-plus"></i>View Enqueryes</a></li>
+                    <li class="list"><a href="<?php echo base_url('enquiry/enquirydetails') ?>"><i class="fa fa-plus"></i>View Enqueryes</a></li>
                 </ul>
             </li>
             <li class="treeview">
