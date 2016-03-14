a6fb5d2b168bd1da61e9494c9dd9900b0c21b12c
amttm <amt.tmg@gmail.com>
2016-03-14 10:55:25 +0545

    footer change

--- application/views/pages/customers/customerdetails.php
+++ application/views/pages/customers/customerdetails.php
@@ -428,7 +428,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Modal title</h4>
+                <h4 class="modal-title">New Enquery</h4>
             </div>
             <div class="modal-body">
                 <?php echo form_open_multipart('enquiry/addEnquiry', array('id' => 'myform')); ?>
@@ -549,8 +549,9 @@
                 <?php echo(form_close()) ?>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary" id="save">Save changes</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
             </div>
         </div>
 
