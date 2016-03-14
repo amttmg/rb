baf4ae485633913c45b5e907f88dea157a02b07d
amttm <amt.tmg@gmail.com>
2016-03-14 11:37:43 +0545

    header hange

--- application/views/pages/customers/customerdetails.php
+++ application/views/pages/customers/customerdetails.php
@@ -196,7 +196,7 @@
                 <a href="#" id="seemore">See More</a>
                <div class="btn-group pull-right">
 
-                   <a href="#" id="Edit" class=" btn btn-primary btn-sm"><i class="glyphicon glyphicon-edit"></i> Edit</a>
+                   <a href="#" id="btneditcustomer" class=" btn btn-primary btn-sm"><i class="glyphicon glyphicon-edit"></i> Edit</a>
                    <a href="#" id="deactive" class=" btn btn-danger btn-sm"><i class="glyphicon glyphicon-remove"></i> Deactive</a>
                    </div>
 
@@ -565,6 +565,27 @@
     </div>
 </div>
 
+<div class="modal fade" tabindex="-1" role="dialog" id=editcustomer>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 <script>
     $('#btnAddCard').click(function () {
         $('#modaladdcard').modal();
@@ -582,6 +603,9 @@
 
     $('#btnnewenq').click(function(){
         $('#newEnquiry').modal('show');
+    })
+    $('#btneditcustomer').click(function(){
+        $('#editcustomer').modal('show')
     })
 </script>
 <script type="text/javascript">
