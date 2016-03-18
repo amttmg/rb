f4209c39913f33703ae91ea1567c26d8d482a9c0
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-03-18 17:13:29 +0545

    add stone form

--- application/views/pages/stones/stone_view.php
+++ application/views/pages/stones/stone_view.php
@@ -117,7 +117,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Modal title</h4>
+                <h4 class="modal-title">Edit Stone</h4>
             </div>
             <div class="modal-body">
                 <form action="" method="POST" role="form" id="edit_stone_form">
