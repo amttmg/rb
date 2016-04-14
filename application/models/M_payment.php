dd4076cd7619864a3591735681a64aee743d161a
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-04-14 12:11:07 +0545

    order taged and add to product completed

---
+++ application/models/M_payment.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_payment extends CI_Model {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function insert($order_id,$type)
+	{
+		$data=array();
+		if ($type=='advance') 
+		{
+			$data=array(
+			'amount'=>$this->input->post('discount'),
+			'order_id'=>$order_id,
+			'type'=>$type,
+			'created_at'=>getCurrentDateTime(),
+			'user_id'=>$this->ion_auth->get_user_id()
+			);
+		}
+		
+		$this->db->insert('tbl_payments',$data);
+	}
+
+	
+
+}
+
+/* End of file M_payment.php */
+/* Location: ./application/models/M_payment.php */
