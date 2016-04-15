d7a6d5ea39dcafabb85fb548b8d8b81940887a1b
amttm <amt.tmg@gmail.com>
2016-04-15 14:34:03 +0545

    Sales View

--- application/models/M_sales.php
+++ application/models/M_sales.php
@@ -6,7 +6,18 @@
  * Date: 4/15/2016
  * Time: 2:25 PM
  */
-class M_sales
+class M_sales extends CI_Model
 {
-
+    function getSales($parameters = '')
+    {
+        $sql = "select * from tbl_sales where 1=1";
+        if (is_array($parameters)) {
+            foreach ($parameters as $key => $value) {
+                $sql .= " and " . $key . "=" . $value;
+            }
+        }
+        $query = $this->db->query($sql);
+        $data = $query->result();
+        return $data;
+    }
 }
