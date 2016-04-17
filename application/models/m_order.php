2cd9bfa56aa87fc0c577197a11a34958c7f17e1d
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-04-17 17:06:32 +0545

    sales

--- application/models/m_order.php
+++ application/models/m_order.php
@@ -30,7 +30,7 @@ class M_order extends CI_Model
 
     public function getActiveOrdersByCustomer($customer_id)
     {
-        $sql = "SELECT * FROM (tbl_order_details INNER JOIN tbl_orders on tbl_orders.order_id=tbl_order_details.order_id) inner JOIN tbl_products on tbl_products.order_no=tbl_order_details.order_no WHERE tbl_orders.customer_id='$customer_id' and tbl_order_details.status=1 and tbl_products.status!='sold'";
+        $sql = "SELECT * FROM (tbl_order_details INNER JOIN tbl_orders on tbl_orders.order_id=tbl_order_details.order_id) inner JOIN tbl_products on tbl_products.order_no=tbl_order_details.order_no WHERE tbl_orders.customer_id='$customer_id' and tbl_order_details.status=1 and tbl_products.status!='sold' ";
         $qry = $this->db->query($sql);
         $result = $qry->result();
         return $result;
@@ -43,7 +43,7 @@ class M_order extends CI_Model
         $this->db->from('tbl_products');
         $this->db->join('tbl_product_category','tbl_product_category.category_id=tbl_products.category_id');
         $this->db->where('tbl_products.model_no',$model_no);
-        //$this->db->where("tbl_products.status !='sold'",null);
+        $this->db->where("tbl_products.status !=",'sold');
         $result = $this->db->get()->result();
         return $result;
     }
