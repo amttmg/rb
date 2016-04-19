5b2645fa667d96c985b3a93aad9427f4256d1d5e
dipesh pokhrel <dipeshpokhrel36@gmail.com>
2016-04-19 15:32:14 +0545

    corrected metal update form

--- application/controllers/Metal.php
+++ application/controllers/Metal.php
@@ -59,7 +59,14 @@ class Metal extends CI_Controller {
 		$master['status'] = True;
         $data = array();
         $master = array();
-		$this->form_validation->set_rules('metal', 'Metal Name', 'trim|required|max_length[64]');
+        $metal=$this->db->from('tbl_metals')->where('metal_id',$id)->get()->result();
+        $is_unique='';
+        if($this->input->post('metal')!=$metal[0]->metal)
+        {
+        	$is_unique =  '|is_unique[tbl_metals.metal]';
+        }
+
+		$this->form_validation->set_rules('metal', 'Metal Name', 'trim|required|max_length[64]'.$is_unique);
 		$this->form_validation->set_rules('metaltype', 'Metal Type', 'callback_dropdown_fun');
 		$this->form_validation->set_rules('unit', 'Unit', 'trim|required|max_length[64]');
 		$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
